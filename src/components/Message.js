1341101efab5230a816f7b28c8f56263a282b48a
austinmpask <austinmpask@gmail.com>
2024-03-01 13:33:25 -0800

    done

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,11 +1,39 @@
-function Message({ size }) {
-  console.log('Message', size);
+import { useEffect, useState } from "react";
 
-  return (
-    <div className="message medium">
-      (Oh my! Your bird is naked!)
-    </div>
-  );
-};
+function Message({ size, featherCount }) {
+  const [sizeClass, setSizeClass] = useState("");
+  const [message, setMessage] = useState("");
+  useEffect(() => {
+    console.log("Message", size);
+    let cname = "";
+    switch (size) {
+      case "s":
+        cname = "small";
+        break;
+      case "m":
+        cname = "medium";
+        break;
+      case "l":
+        cname = "large";
+        break;
+      case "xl":
+        cname = "xlarge";
+        break;
+    }
+    setSizeClass(cname);
+  }, [size]);
+
+  useEffect(() => {
+    if (featherCount <= 0) {
+      setMessage("Naked bird moment");
+    } else if (featherCount <= 4) {
+      setMessage("Kinda cold tbh");
+    } else {
+      setMessage("v warm");
+    }
+  }, [featherCount]);
+
+  return <div className={`message ${sizeClass}`}>{message}</div>;
+}
 
 export default Message;
--- src/components/PictureDisplay.js
+++ src/components/PictureDisplay.js
@@ -9,6 +9,7 @@ import feather7 from "../images/feather7.svg";
 import feather8 from "../images/feather8.svg";
 import feather9 from "../images/feather9.svg";
 import featherA from "../images/featherA.svg";
+import { useEffect, useState } from "react";
 
 const feathers = [
   feather1,
@@ -23,26 +24,54 @@ const feathers = [
   featherA,
 ];
 
-function PictureDisplay ({ size, featherCount, featherColors }) {
-  console.log('PictureDisplay', size, featherCount, featherColors);
+function PictureDisplay({ size, featherCount, featherColors }) {
+  const [sizeClass, setSizeClass] = useState("");
+
+  useEffect(() => {
+    console.log("PictureDisplay size", size);
+    let cname = "";
+    switch (size) {
+      case "s":
+        cname = "small";
+        break;
+      case "m":
+        cname = "medium";
+        break;
+      case "l":
+        cname = "large";
+        break;
+      case "xl":
+        cname = "xlarge";
+        break;
+    }
+    setSizeClass(cname);
+  }, [size]);
+
+  useEffect(() => {
+    console.log("PictureDisplay feather count", featherCount);
+  }, [featherCount]);
+
+  useEffect(() => {
+    console.log("PictureDisplay feather colors", featherColors);
+  }, [featherColors]);
 
   // TODO: Wrap in useEffect
   const colors = [];
-  if (!featherColors || featherColors.length === 0) featherColors = [''];
-  for (let i=0; i<featherCount; i++) {
+  if (!featherColors || featherColors.length === 0) featherColors = [""];
+  for (let i = 0; i < featherCount; i++) {
     colors.push(featherColors[i % featherColors.length]);
   }
 
   return (
-    <div className={`image-area medium`}>
-      {colors.map((c, i) =>
-        <img  
-          key={feathers[i]} 
-          src={feathers[i]} 
-          className={`image-feather ${c}`} 
-          alt="" 
+    <div className={`image-area ${sizeClass}`}>
+      {colors.map((c, i) => (
+        <img
+          key={feathers[i]}
+          src={feathers[i]}
+          className={`image-feather ${c}`}
+          alt=""
         />
-      )}
+      ))}
 
       <img src={turkey} className="image-turkey" alt="turkey" />
     </div>
